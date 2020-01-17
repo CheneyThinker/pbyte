4d1b1a36bd38fadf82617e350e078c59cba369e2
CheneyThinker <clxuanye@163.com>
2020-01-17 12:49:35 +0800

    Modified By CheneyThinker

--- include/pebyte_base.h
+++ include/pebyte_base.h
@@ -5,47 +5,68 @@
 
 #include "pebyte_def.h"
 
-#define PRINTF_BYTE(declared)                    \
-    byte declared;                               \
-    fread(&declared, 1, sizeof(byte), pReadFile);\
-    printf(#declared": %02x\n", declared);
+#define PRINTF_BYTE(declared)                          \
+          byte declared;                               \
+          fread(&declared, 1, sizeof(byte), pReadFile);\
+          printf(#declared": %02x\n", declared);
 
-#define PRINTF_WORD(declared)                    \
-    word declared;                               \
-    fread(&declared, 1, sizeof(word), pReadFile);\
-    printf(#declared": %04x\n", declared);
+#define PRINTF_WORD(declared)                          \
+          word declared;                               \
+          fread(&declared, 1, sizeof(word), pReadFile);\
+          printf(#declared": %04x\n", declared);
 
-#define PRINTF_DWORD(declared)                    \
-    dword declared;                               \
-    fread(&declared, 1, sizeof(dword), pReadFile);\
-    printf(#declared": %08x\n", declared);
+#define PRINTF_DWORD(declared)                          \
+          dword declared;                               \
+          fread(&declared, 1, sizeof(dword), pReadFile);\
+          printf(#declared": %08x\n", declared);
 
-#define PRINTF_QWORD(declared)                    \
-    qword declared;                               \
-    fread(&declared, 1, sizeof(qword), pReadFile);\
-    printf(#declared": %016lx\n", declared);
+#define PRINTF_QWORD(declared)                          \
+          qword declared;                               \
+          fread(&declared, 1, sizeof(qword), pReadFile);\
+          printf(#declared": %016lx\n", declared);
 
-#define PRINTF_PWORD(declared)                  \
-    fread(declared, 1, sizeof(word), pReadFile);\
-    printf(#declared": %04x\n", *declared);
+#define PRINTF_PWORD(declared)                        \
+          fread(declared, 1, sizeof(word), pReadFile);\
+          printf(#declared": %04x\n", *declared);
 
-#define PRINTF_PDWORD(declared)                  \
-    fread(declared, 1, sizeof(dword), pReadFile);\
-    printf(#declared": %08x\n", *declared);
+#define PRINTF_PDWORD(declared)                        \
+          fread(declared, 1, sizeof(dword), pReadFile);\
+          printf(#declared": %08x\n", *declared);
 
-#define PRINTF_WORD_ARR(declared, size)            \
-    word declared;                                 \
-    printf(#declared":");                          \
-    for (word index = 0; index < size; index++)    \
-    {                                              \
-      fread(&declared, 1, sizeof(word), pReadFile);\
-      printf(" %04x", declared);                   \
-    }                                              \
-    printf("\n");
+#define PRINTF_BYTE_ARR(declared, size)                  \
+          byte declared;                                 \
+          printf(#declared":");                          \
+          for (byte index = 0; index < size; index++)    \
+          {                                              \
+            fread(&declared, 1, sizeof(byte), pReadFile);\
+            printf(" %02x", declared);                   \
+          }                                              \
+          printf("\n");
 
-#define PRINTF_PDWORD_ARR(declared, index)               \
-    fread(&declared[index], 1, sizeof(dword), pReadFile);\
-    printf(#declared": %08x\n", declared[index]);
+#define PRINTF_WORD_ARR(declared, size)                  \
+          word declared;                                 \
+          printf(#declared":");                          \
+          for (word index = 0; index < size; index++)    \
+          {                                              \
+            fread(&declared, 1, sizeof(word), pReadFile);\
+            printf(" %04x", declared);                   \
+          }                                              \
+          printf("\n");
+
+#define PRINTF_PDWORD_ARR(declared, index)                     \
+          fread(&declared[index], 1, sizeof(dword), pReadFile);\
+          printf(#declared": %08x\n", declared[index]);
+
+#define PRINTF_STRING(declared)                                             \
+          {                                                                 \
+            byte declared;                                                  \
+            printf(#declared": ");                                          \
+            while (fread(&declared, 1, sizeof(byte), pReadFile) && declared)\
+            {                                                               \
+              printf("%c", declared);                                       \
+            }                                                               \
+            printf("\n");                                                   \
+          }
 
 dword rva2foa(dword, word, word, dword*, dword*, dword*);
 
--- src/pebyte_analyzer.c
+++ src/pebyte_analyzer.c
@@ -16,9 +16,10 @@ int pebyte_analyzer(int argc, char** argv)
     word sizeOfOptionalHeader;
     coff_file_header(pReadFile, &numberOfSections, &sizeOfOptionalHeader);
 
+    word  magic;
     dword sectionAlignment;
     dword numberOfRvaAndSizes;
-    optional_header(pReadFile, &sectionAlignment, &numberOfRvaAndSizes);
+    optional_header(pReadFile, &magic, &sectionAlignment, &numberOfRvaAndSizes);
 
     dword virtualAddress[numberOfRvaAndSizes];
     dword size[numberOfRvaAndSizes];
@@ -29,16 +30,16 @@ int pebyte_analyzer(int argc, char** argv)
     dword pointerToRawData[numberOfSections];
     section_table(pReadFile, numberOfSections, sectionVirtualAddress, sizeOfRawData, pointerToRawData);
 
-    optional_header_data_directories_item(pReadFile, numberOfRvaAndSizes, virtualAddress, size, sectionAlignment, numberOfSections, sectionVirtualAddress, sizeOfRawData, pointerToRawData);
+    optional_header_data_directories_item(pReadFile, magic, numberOfRvaAndSizes, virtualAddress, size, sectionAlignment, numberOfSections, sectionVirtualAddress, sizeOfRawData, pointerToRawData);
 
   }
   fclose(pReadFile);
   return 0;
 }
 
-void image_dos_header(FILE* pReadFile, dword* e_lfanew)
+void dos_header(FILE* pReadFile, dword* e_lfanew)
 {
-  printf("***image_dos_header***\n");
+  printf("***dos_header***\n");
   PRINTF_WORD(e_magic)
   PRINTF_WORD(e_cblp)
   PRINTF_WORD(e_cp)
@@ -58,7 +59,7 @@ void image_dos_header(FILE* pReadFile, dword* e_lfanew)
   PRINTF_WORD(e_oeminfo)
   PRINTF_WORD_ARR(e_res2, E_RES2_SIZE)
   PRINTF_PDWORD(e_lfanew)
-  printf("***image_dos_header***\n");
+  printf("***dos_header***\n");
 }
 
 void ms_dos_stub(FILE* pReadFile, dword e_lfanew)
@@ -105,11 +106,11 @@ void coff_file_header(FILE* pReadFile, word* numberOfSections, word* sizeOfOptio
   printf("***coff_file_header***\n");
 }
 
-void optional_header(FILE* pReadFile, dword* sectionAlignment, dword* numberOfRvaAndSizes)
+void optional_header(FILE* pReadFile, word* magic, dword* sectionAlignment, dword* numberOfRvaAndSizes)
 {
   printf("***optional_header***\n");
   printf("***optional_header standard_fields***\n");
-  PRINTF_WORD(magic)
+  PRINTF_PWORD(magic)
   PRINTF_BYTE(majorLinkerVersion)
   PRINTF_BYTE(minorLinkerVersion)
   PRINTF_DWORD(sizeOfCode)
@@ -117,14 +118,14 @@ void optional_header(FILE* pReadFile, dword* sectionAlignment, dword* numberOfRv
   PRINTF_DWORD(sizeOfUninitializedData)
   PRINTF_DWORD(addressOfEntryPoint)
   PRINTF_DWORD(baseOfCode)
-  if (magic == 0x010b)
+  if (*magic == 0x010b)
   {
     PRINTF_DWORD(baseOfData)
     printf("***optional_header standard_fields***\n");
     printf("***optional_header windows_specific_fields***\n");
     PRINTF_DWORD(imageBase)
   }
-  else if (magic == 0x020b)
+  else if (*magic == 0x020b)
   {
     printf("***optional_header standard_fields***\n");
     printf("***optional_header windows_specific_fields***\n");
@@ -144,14 +145,14 @@ void optional_header(FILE* pReadFile, dword* sectionAlignment, dword* numberOfRv
   PRINTF_DWORD(checkSum)
   PRINTF_WORD(subsystem)
   PRINTF_WORD(dllCharacteristics)
-  if (magic == 0x010b)
+  if (*magic == 0x010b)
   {
     PRINTF_DWORD(sizeOfStackReserve)
     PRINTF_DWORD(sizeOfStackCommit)
     PRINTF_DWORD(sizeOfHeapReserve)
     PRINTF_DWORD(sizeOfHeapCommit)
   }
-  else if (magic == 0x020b)
+  else if (*magic == 0x020b)
   {
     PRINTF_QWORD(sizeOfStackReserve)
     PRINTF_QWORD(sizeOfStackCommit)
@@ -196,7 +197,7 @@ void section_table(FILE* pReadFile, word numberOfSections, dword* virtualAddress
   printf("***section_table***\n");
 }
 
-void optional_header_data_directories_item(FILE* pReadFile, dword numberOfRvaAndSizes, dword* virtualAddress, dword* size, dword sectionAlignment, word numberOfSections, dword* sectionVirtualAddress, dword* sizeOfRawData, dword* pointerToRawData)
+void optional_header_data_directories_item(FILE* pReadFile, word magic, dword numberOfRvaAndSizes, dword* virtualAddress, dword* size, dword sectionAlignment, word numberOfSections, dword* sectionVirtualAddress, dword* sizeOfRawData, dword* pointerToRawData)
 {
   printf("***optional_header_data_directories_item***\n");
   for (dword index = 0x00000000; index < numberOfRvaAndSizes; index = index + 0x00000001)
@@ -213,7 +214,7 @@ void optional_header_data_directories_item(FILE* pReadFile, dword numberOfRvaAnd
           export_table(pReadFile);
           break;
         case 0x00000001:
-          import_table(pReadFile, sectionAlignment, numberOfSections, sectionVirtualAddress, sizeOfRawData, pointerToRawData);
+          import_table(pReadFile, sectionAlignment, numberOfSections, sectionVirtualAddress, sizeOfRawData, pointerToRawData, magic);
           break;
         case 0x00000002:
           resource_table(pReadFile);
@@ -240,7 +241,7 @@ void optional_header_data_directories_item(FILE* pReadFile, dword numberOfRvaAnd
           tls_table(pReadFile);
           break;
         case 0x0000000a:
-          load_config_table(pReadFile);
+          load_config_table(pReadFile, magic);
           break;
         case 0x0000000b:
           bound_import(pReadFile);
@@ -280,12 +281,7 @@ void export_table(FILE* pReadFile)
   PRINTF_DWORD(addressOfNameOrdinals)
 
   fseek(pReadFile, name, SEEK_SET);
-  byte name_str;
-  while (fread(&name_str, 1, 1, pReadFile) && name_str)
-  {
-    printf("%c", name_str);
-  }
-  printf("\n");
+  PRINTF_STRING(name)
 
   fseek(pReadFile, addressOfFunctions, SEEK_SET);
   dword addressOfFunctions_[numberOfFunctions];
@@ -314,11 +310,7 @@ void export_table(FILE* pReadFile)
   {
     printf("%08x ", addressOfNames_[index]);
     fseek(pReadFile, addressOfNames_[index], SEEK_SET);
-    while (fread(&name_str, 1, 1, pReadFile) && name_str)
-    {
-      printf("%c", name_str);
-    }
-    printf("\n");
+    PRINTF_STRING(name)
   }
 
   fseek(pReadFile, addressOfNameOrdinals, SEEK_SET);
@@ -341,29 +333,23 @@ void export_table(FILE* pReadFile)
   printf("***optional_header_data_directories_item export_table***\n");
 }
 
-void import_table(FILE* pReadFile, dword sectionAlignment, word numberOfSections, dword* sectionVirtualAddress, dword* sizeOfRawData, dword* pointerToRawData)
+void import_table(FILE* pReadFile, dword sectionAlignment, word numberOfSections, dword* sectionVirtualAddress, dword* sizeOfRawData, dword* pointerToRawData, word magic)
 {
   printf("***optional_header_data_directories_item import_table***\n");
+  fpos_t pos;
   while (1)
   {
-    PRINTF_DWORD(characteristics)
+    PRINTF_DWORD(characteristics)//Import Lookup Table RVA
     PRINTF_DWORD(timeDateStamp)
     PRINTF_DWORD(forwarderChain)
-    PRINTF_DWORD(name)
-    PRINTF_DWORD(firstThunk)
+    PRINTF_DWORD(name)//Name RVA
+    PRINTF_DWORD(firstThunk)//Import Address Table RVA
     if (characteristics || timeDateStamp || forwarderChain || name || firstThunk)
     {
-      fpos_t pos;
       fgetpos(pReadFile, &pos);
-      dword name_foa = rva2foa(name, sectionAlignment, numberOfSections, sectionVirtualAddress, sizeOfRawData, pointerToRawData);
-      fseek(pReadFile, name_foa, SEEK_SET);
-      byte name_str;
-      while (fread(&name_str, 1, 1, pReadFile) && name_str)
-      {
-        printf("%c", name_str);
-      }
-      printf("\n");
-      //image_thunk_data(pReadFile, characteristics);//originalFirstThunk = characteristics
+      fseek(pReadFile, rva2foa(name, sectionAlignment, numberOfSections, sectionVirtualAddress, sizeOfRawData, pointerToRawData), SEEK_SET);
+      //PRINTF_STRING(name)
+      thunk_data(pReadFile, characteristics);//originalFirstThunk = characteristics
       fsetpos(pReadFile, &pos);
     }
     else
@@ -377,6 +363,7 @@ void import_table(FILE* pReadFile, dword sectionAlignment, word numberOfSections
 void resource_table(FILE* pReadFile)
 {
   printf("***optional_header_data_directories_item resource_table***\n");
+  fpos_t pos;
   while (1)
   {
     PRINTF_DWORD(characteristics)
@@ -387,6 +374,12 @@ void resource_table(FILE* pReadFile)
     PRINTF_WORD(numberOfIdEntries)
     if (characteristics || timeDateStamp || majorVersion || minorVersion || numberOfNamedEntries || numberOfIdEntries)
     {
+      if (numberOfNamedEntries || numberOfIdEntries)
+      {
+        fgetpos(pReadFile, &pos);
+        resource_directory_entry(pReadFile, numberOfNamedEntries + numberOfIdEntries);
+        fsetpos(pReadFile, &pos);
+      }
     }
     else
     {
@@ -411,6 +404,14 @@ void certificate_table(FILE* pReadFile)
 void base_relocation_table(FILE* pReadFile)
 {
   printf("***optional_header_data_directories_item base_relocation_table***\n");
+  PRINTF_DWORD(virtualAddress)
+  PRINTF_DWORD(sizeOfBlock)
+  dword typeOffsetCount = (sizeOfBlock - 0x00000008) / 2;
+  while (typeOffsetCount)
+  {
+    PRINTF_WORD(typeOffset)
+    typeOffsetCount = typeOffsetCount - 0x00000001;
+  }
   printf("***optional_header_data_directories_item base_relocation_table***\n");
 }
 
@@ -438,9 +439,75 @@ void tls_table(FILE* pReadFile)
   printf("***optional_header_data_directories_item tls_table***\n");
 }
 
-void load_config_table(FILE* pReadFile)
+void load_config_table(FILE* pReadFile, word magic)
 {
   printf("***optional_header_data_directories_item load_config_table***\n");
+  PRINTF_DWORD(characteristics)
+  PRINTF_DWORD(timeDateStamp)
+  PRINTF_WORD(majorVersion)
+  PRINTF_WORD(minorVersion)
+  PRINTF_DWORD(globalFlagsClear)
+  PRINTF_DWORD(globalFlagsSet)
+  PRINTF_DWORD(criticalSectionDefaultTimeout)
+  if (magic == 0x010b)
+  {
+    PRINTF_DWORD(deCommitFreeBlockThreshold)
+    PRINTF_DWORD(deCommitTotalFreeThreshold)
+    PRINTF_DWORD(lockPrefixTable)
+    PRINTF_DWORD(maximumAllocationSize)
+    PRINTF_DWORD(virtualMemoryThreshold)
+    PRINTF_DWORD(processAffinityMask)
+  }
+  else if (magic == 0x020b)
+  {
+    PRINTF_QWORD(deCommitFreeBlockThreshold)
+    PRINTF_QWORD(deCommitTotalFreeThreshold)
+    PRINTF_QWORD(lockPrefixTable)
+    PRINTF_QWORD(maximumAllocationSize)
+    PRINTF_QWORD(virtualMemoryThreshold)
+    PRINTF_QWORD(processAffinityMask)
+  }
+  PRINTF_DWORD(processHeapFlags)
+  PRINTF_WORD(csdVersion)
+  PRINTF_WORD(reserved)
+  if (magic == 0x010b)
+  {
+    PRINTF_DWORD(editList)
+    PRINTF_DWORD(securityCookie)
+    PRINTF_DWORD(seHandlerTable)
+    PRINTF_DWORD(seHandlerCount)
+    PRINTF_DWORD(guardCFCheckFunctionPointer)
+    PRINTF_DWORD(guardCFDispatchFunctionPointer)
+    PRINTF_DWORD(guardCFFunctionTable)
+    PRINTF_DWORD(guardCFFunctionCount)
+  }
+  else if (magic == 0x020b)
+  {
+    PRINTF_QWORD(editList)
+    PRINTF_QWORD(securityCookie)
+    PRINTF_QWORD(seHandlerTable)
+    PRINTF_QWORD(seHandlerCount)
+    PRINTF_QWORD(guardCFCheckFunctionPointer)
+    PRINTF_QWORD(guardCFDispatchFunctionPointer)
+    PRINTF_QWORD(guardCFFunctionTable)
+    PRINTF_QWORD(guardCFFunctionCount)
+  }
+  PRINTF_DWORD(guardFlags)
+  PRINTF_BYTE_ARR(codeIntegrity, 12);
+  if (magic == 0x010b)
+  {
+    PRINTF_DWORD(guardAddressTakenIatEntryTable)
+    PRINTF_DWORD(guardAddressTakenIatEntryCount)
+    PRINTF_DWORD(guardLongJumpTargetTable)
+    PRINTF_DWORD(guardLongJumpTargetCount)
+  }
+  else if (magic == 0x020b)
+  {
+    PRINTF_QWORD(guardAddressTakenIatEntryTable)
+    PRINTF_QWORD(guardAddressTakenIatEntryCount)
+    PRINTF_QWORD(guardLongJumpTargetTable)
+    PRINTF_QWORD(guardLongJumpTargetCount)
+  }
   printf("***optional_header_data_directories_item load_config_table***\n");
 }
 
@@ -474,42 +541,38 @@ void reserved(FILE* pReadFile)
   printf("***optional_header_data_directories_item reserved***\n");
 }
 
-/*
-void image_thunk_data(FILE* pReadFile, dword originalFirstThunk)
+void thunk_data(FILE* pReadFile, dword originalFirstThunk)
 {
-  printf("\t\t\t\t---IMAGE_THUNK_DATA---\n");
+  printf("***optional_header_data_directories_item import_table thunk_data***\n");
+  fpos_t pos;
   fseek(pReadFile, originalFirstThunk, SEEK_SET);
-  IMAGE_THUNK_DATA itd;
-  byte counter = 0x00;
-  fpos_t position;
-  while (fread(&itd, 1, sizeof(IMAGE_THUNK_DATA), pReadFile) && itd.u1.forwarderString)
+  while (1)
   {
-    if (counter == 0x00)
+    PRINTF_DWORD(forwarderString_function_ordinal_addressOfData)
+    if (forwarderString_function_ordinal_addressOfData)
     {
-      counter = counter + 0x01;
-      printf("\t\t\t\t%08x", itd.u1.forwarderString);//itd.u1.function, itd.u1.ordinal, itd.u1.addressOfData
-    }
-    else if (counter == 0x09)
-    {
-      counter = 0x00;
-      printf(" %08x\n", itd.u1.forwarderString);
+      fgetpos(pReadFile, &pos);
+      fseek(pReadFile, forwarderString_function_ordinal_addressOfData, SEEK_SET);
+      PRINTF_WORD(hint)
+      PRINTF_STRING(name)
+      fsetpos(pReadFile, &pos);
     }
     else
     {
-      counter = counter + 0x01;
-      printf(" %08x", itd.u1.forwarderString);
+      break;
     }
-    fgetpos(pReadFile, &position);
-    fseek(pReadFile, itd.u1.forwarderString, SEEK_SET);
-    IMAGE_IMPORT_BY_NAME iibn;
-    fread(&iibn, 1, sizeof(IMAGE_IMPORT_BY_NAME), pReadFile);
-    printf("%04x\n", iibn.hint);
-    fsetpos(pReadFile, &position);
   }
-  if (counter)
-  {
-    printf("\n");
-  }
-  printf("\t\t\t\t---IMAGE_THUNK_DATA---\n");
+  printf("***optional_header_data_directories_item import_table thunk_data***\n");
 }
-*/
+
+void resource_directory_entry(FILE* pReadFile, dword size)
+{
+  printf("***optional_header_data_directories_item resource_table resource_directory_entry***\n");
+  //while (size)
+  //{
+    PRINTF_DWORD(name)
+    PRINTF_DWORD(offsetToData)
+    //size = size - 0x00000001;
+  //}
+  printf("***optional_header_data_directories_item resource_table resource_directory_entry***\n");
+}
